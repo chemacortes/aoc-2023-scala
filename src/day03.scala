9d06c3e4160146a95d457535a8bf379568be2508
chemacortes <devxtrem@gmail.com>
2023-12-05 02:55:42 +0100

    Solution of day 03 (only part1)

---
+++ src/day03.scala
@@ -0,0 +1,61 @@
+package day03
+
+import locations.Directory.currentDir
+import inputs.Input.loadFileSync
+import scala.util.matching.Regex.Match
+
+@main def part1: Unit =
+  println(s"The solution is ${part1(loadInput())}")
+
+@main def part2: Unit =
+  println(s"The solution is ${part2(loadInput())}")
+
+//def loadInput(): String = loadFileSync(s"$currentDir/../input/day00")
+def loadInput(): String = loadFileSync(s"$currentDir/../input/day03")
+
+case class Coord(x: Int, y: Int):
+    def withIn(start: Coord, end: Coord) =
+      if y < start.y || y > end.y then false
+      else if x < start.x || x > end.x then false
+      else true
+
+case class PartNumber(value: Int, start: Coord, end: Coord)
+case class Symbol(sym: String, pos: Coord):
+    def neighborOf(number: PartNumber) =
+      pos.withIn(
+        Coord(number.start.x - 1, number.start.y - 1),
+        Coord(number.end.x + 1, number.end.y + 1)
+      )
+
+object IsInt:
+    def unapply(in: Match): Option[Int] =
+      in.matched.toIntOption
+
+def findPartsAndSymbols(input: String) =
+  input
+    .split("\n")
+    .zipWithIndex
+    .flatMap: (line, i) =>
+        """(\d+|[^.\d])""".r
+          .findAllMatchIn(line)
+          .map:
+              case m @ IsInt(value) =>
+                PartNumber(value, Coord(m.start, i), Coord(m.end - 1, i))
+              case s => Symbol(s.matched, Coord(s.start, i))
+
+def part1(input: String): String =
+
+    val all = findPartsAndSymbols(loadInput())
+    val symbols = all.collect { case s: Symbol => s }
+
+    all
+      .collect:
+          case n: PartNumber if symbols.exists(_.neighborOf(n)) => n.value
+      .sum
+      .toString
+
+end part1
+
+def part2(input: String): String =
+  ???
+end part2
