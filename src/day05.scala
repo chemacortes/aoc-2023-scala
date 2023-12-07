e035837235dcdb4aede45c4832f0813342dd1dbe
chemacortes <devxtrem@gmail.com>
2023-12-07 20:15:31 +0100

    Solution of day05 (part1). Rewritten face to part2

--- src/day05.scala
+++ src/day05.scala
@@ -4,57 +4,65 @@ import locations.Directory.currentDir
 import inputs.Input.loadFileSync
 
 @main def part1: Unit =
-  println(s"The solution is ${part1(loadInput())}")
+  println(s"The solution is ${part1(loadInput())}") // 107430936
 
 @main def part2: Unit =
   println(s"The solution is ${part2(loadInput())}")
 
 def loadInput(): String = loadFileSync(s"$currentDir/../input/day05")
 
-case class MapRange(dest: Long, source: Long, range: Long):
-    def contains(n: Long) = source <= n && n < source + range
-    def map(n: Long): Long = n - source + dest
+case class Range(start: Long, range: Long):
+    val last = start + range - 1
 
-case class AlmanacMap(name: String, ranges: List[MapRange]):
+    // Used in part1
+    def contains(n: Long) = start <= n && n <= last
+    def map(dest: Long, n: Long) = n - start + dest
+
+    // Used in part2
+    def isEmpty = range == 0
+    def map(dest: Long, r: Range): (List[Range], List[Range]) = // Used in part2
+      ???
+
+case class AlmanacMap(name: String, mapping: List[(Long, Range)]):
     def map(n: Long) =
-      ranges.find(r => r contains n) match
-          case Some(r) => r.map(n)
-          case None    => n
+      mapping.find((_, m) => m contains n) match
+          case Some((dest, m)) => m.map(dest, n)
+          case None            => n
 
 def getSeeds(line: String) =
     val s"seeds: $seeds0" = line: @unchecked
     """\d+""".r.findAllIn(seeds0).map(_.toLong).toList
 
 def getMappings(text: String) =
+  /* Almanac Maps:
+      - seed-to-soil
+      - soil-to-fertilizer
+      - fertilizer-to-water
+      - water-to-light
+      - light-to-temperature
+      - temperature-to-humidity
+      - humidity-to-location
+   */
   """(?s)[-a-z]+ map:[\n 0-9]+""".r
     .findAllIn(text)
     .map: block =>
         val it = block.linesIterator
         val s"$name map:" = it.next(): @unchecked
-        val maps =
+        val mappings =
           it
-            .withFilter(_.length > 0)
+            .withFilter(_.length > 0) // wipe empty lines
             .map: line =>
                 val s"$dest $source $range" = line: @unchecked
-                MapRange(dest.toLong, source.toLong, range.toLong)
-        AlmanacMap(name, maps.toList)
+                (dest.toLong, Range(source.toLong, range.toLong))
+        AlmanacMap(name, mappings.toList)
     .toList
-
-val almanacMaps =
-  """
-  seed-to-soil
-  soil-to-fertilizer
-  fertilizer-to-water
-  water-to-light
-  light-to-temperature
-  temperature-to-humidity
-  humidity-to-location
-  """
 
 def part1(input: String): String =
     val input = loadInput()
-    val seeds = getSeeds(input.split("\n")(0))
-    val mappings = getMappings(input)
+
+    val Array(seedsLine, almanacMaps) = input.split("\n", 2)
+    val seeds = getSeeds(seedsLine)
+    val mappings = getMappings(almanacMaps)
 
     seeds
       .map: seed =>
