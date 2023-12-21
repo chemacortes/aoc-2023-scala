955c1dc83f99eef46b8cb86699d7442dc7fd041b
chemacortes <devxtrem@gmail.com>
2023-12-21 19:28:39 +0100

    Solution day12 (part1)

---
+++ src/day12-part1.scala
@@ -0,0 +1,54 @@
+package day12b
+
+import inputs.Input.loadFileSync
+import locations.Directory.currentDir
+
+@main def part1: Unit =
+  println(s"The solution is ${part1(loadInput())}")
+
+@main def part2: Unit =
+  println(s"The solution is ${part2(loadInput())}")
+
+def loadInput(): String = loadFileSync(s"$currentDir/../input/day12.txt")
+
+case class Condition(pattern: String, groups: Seq[Int]):
+
+    def allCombinations(pat: String): Seq[String] =
+      pat match
+          case ""  => Seq("")
+          case "?" => Seq(".", "#")
+          case s"?$tail" =>
+            for
+                p <- allCombinations(tail)
+                c <- ".#"
+            yield c +: p
+          case _ =>
+            for p <- allCombinations(pat.tail)
+            yield pat.head +: p
+
+    def possibleGroups =
+      allCombinations(pattern)
+        .map: pat =>
+            """#+""".r
+              .findAllIn(pat)
+              .map(_.size)
+              .toSeq
+        .count(_ == groups)
+
+object Condition:
+    def parse(s: String) =
+        val Array(pat, g) = s.split(" ")
+        Condition(pat, g.split(",").map(_.toInt))
+
+def part1(input: String): String =
+  input.linesIterator
+    .map: line =>
+        Condition.parse(line).possibleGroups
+    .sum
+    .toString
+
+end part1
+
+def part2(input: String): String =
+  ???
+end part2
