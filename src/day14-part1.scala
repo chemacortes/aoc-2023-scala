f1dd373551fe7b49a4b251df8eddfbdd58949ca1
chemacortes <devxtrem@gmail.com>
2023-12-30 18:36:11 +0100

    Solution day14

---
+++ src/day14-part1.scala
@@ -0,0 +1,46 @@
+package day14b
+
+import inputs.Input.loadFileSync
+import locations.Directory.currentDir
+
+@main def part1: Unit =
+  println(s"The solution is ${part1(loadInput())}") // 109654
+
+@main def part2: Unit =
+  println(s"The solution is ${part2(loadInput())}")
+
+def loadInput(): String = loadFileSync(s"$currentDir/../input/day14.txt")
+
+case class Grid(input: Seq[String]):
+
+    def tiltNorth: Grid =
+      Grid(
+        input.transpose
+          .map(_.mkString)
+          .map: line =>
+              line
+                .split("#", -1)
+                .map(_.mkString.sorted.reverse)
+                .mkString("#")
+          .transpose
+          .map(_.mkString)
+      )
+
+    def calculate: Long =
+      input.transpose
+        .map(_.mkString.reverse)
+        .map(_.zipWithIndex.collect:
+            case (c, i) if c == 'O' => i + 1
+        )
+        .map(_.sum.toLong)
+        .sum
+
+def part1(input: String): String =
+    val grid = Grid(input.linesIterator.toSeq)
+    grid.tiltNorth.calculate.toString
+
+end part1
+
+def part2(input: String): String =
+  ???
+end part2
--- src/day14.scala
+++ src/day14.scala
@@ -3,44 +3,81 @@ package day14
 import inputs.Input.loadFileSync
 import locations.Directory.currentDir
 
+import scala.annotation.tailrec
+
 @main def part1: Unit =
   println(s"The solution is ${part1(loadInput())}") // 109654
 
 @main def part2: Unit =
-  println(s"The solution is ${part2(loadInput())}")
+  println(s"The solution is ${part2(loadInput())}") // 94876
 
 def loadInput(): String = loadFileSync(s"$currentDir/../input/day14.txt")
 
-case class Grid(input: Seq[String]):
+// Lighter version of Grid class
+type GridT = Seq[String]
 
-    def tiltNorth: Grid =
-      Grid(
-        input.transpose
-          .map(_.mkString)
-          .map: line =>
-              line
-                .split("#", -1)
-                .map(_.mkString.sorted.reverse)
-                .mkString("#")
-          .transpose
-          .map(_.mkString)
-      )
+extension (grid: GridT)
 
-    def calculate: Long =
-      input.transpose
+    def spin: GridT =
+      grid.transpose
         .map(_.mkString.reverse)
-        .map(_.zipWithIndex.collect:
-            case (c, i) if c == 'O' => i + 1
-        )
-        .map(_.sum.toLong)
+
+    def tilt: GridT =
+      grid
+        .map: line =>
+            line
+              .split("#", -1)
+              .map(_.mkString.sorted)
+              .mkString("#")
+
+    @tailrec
+    def cycle(n: Int = 4): GridT =
+      if n <= 0 then grid else grid.tilt.spin.cycle(n - 1)
+
+object Grid:
+    def parse(input: String): Grid =
+      Grid(input.linesIterator.toSeq.spin)
+
+case class Grid(grid: GridT):
+
+    override def toString(): String = grid.mkString("\n")
+    def spin = Grid(grid.spin)
+    def tilt = Grid(grid.tilt)
+    def cycle(n: Int = 4) = Grid(grid.cycle(n))
+
+    def load: Int =
+      grid
+        .map: line =>
+            line.zipWithIndex
+              .collect:
+                  case (c, i) if c == 'O' => i + 1
+              .sum
         .sum
 
 def part1(input: String): String =
-    val grid = Grid(input.linesIterator.toSeq)
-    grid.tiltNorth.calculate.toString
-
+    val grid = Grid.parse(input)
+    grid.tilt.load.toString
 end part1
 
 def part2(input: String): String =
-  ???
+
+    val CYCLES = 1_000_000_000
+
+    val grid = Grid.parse(input)
+    val states: LazyList[GridT] =
+      LazyList.iterate(grid.grid)(_.cycle())
+
+    // Detecting same state --> cycle
+    val (first, last) =
+      states.zipWithIndex
+        .map((state, n) => (states.take(n).indexOf(state), n))
+        .find((a, _) => a >= 0)
+        .get
+
+    val loopsize = last - first
+    val rest = (CYCLES - first) % loopsize
+    val final_state = states(first + rest)
+
+    Grid(final_state).load.toString
+
 end part2
